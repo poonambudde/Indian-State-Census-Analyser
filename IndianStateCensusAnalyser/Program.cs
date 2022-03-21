af9167e272e8579ca173d94b239176455a758952
poonambudde <buddepoonam123@gmail.com>
2022-03-21 05:41:04 +0530

    [Poonam] Added: Welcome to Indian States Census Analyser Program

---
+++ IndianStateCensusAnalyser/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace IndianStateCensusAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Indian State Census Analyser Program");
+        }
+    }
+}
